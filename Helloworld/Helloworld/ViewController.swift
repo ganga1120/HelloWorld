4ff24dc21b1609c970c7f41a7b0d69ccfff78f9c
osiuser <osiuser@osi-l-290.osius.com>
2017-07-13 11:44:16 +0530

    Intial Commit

---
+++ Helloworld/Helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by osiuser on 13/07/17.
+//  Copyright © 2017 osiuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
